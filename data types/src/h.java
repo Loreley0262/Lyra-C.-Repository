0e0d1e10a1f6ef0f293f7b97abf6b91363b8ec4f
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2019-09-17 14:23:42 -0700

    hhh

---
+++ data types/src/h.java
@@ -0,0 +1,28 @@
+public class h {
+
+    public static void main(String[] args) {
+        int age = 1;
+
+        System.out.println("h");
+        String name = "hello";
+        System.out.println(name);
+        String firstName = "Lyra";
+        String lastName = "Chu";
+        String fullName = firstName + " " + lastName + " age: " + age;
+        System.out.println(fullName);
+
+
+        //the h in hellos is 0, e=1, l=2, l=3, o=4, s=5
+        String hi = "hellos";
+        System.out.println(hi);
+        System.out.println(hi.substring(1,3));
+        //this is the letters in positions 1 to 3..
+
+
+
+
+
+
+    }
+
+}
