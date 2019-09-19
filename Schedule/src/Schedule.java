01c5fc207f4c09ffa264dc846f08e38135cfbbf8
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2019-09-19 13:58:06 -0700

    asdf

---
+++ Schedule/src/Schedule.java
@@ -0,0 +1,53 @@
+public class Schedule {
+    public static void main(String[] args) {
+
+        int a = 113;
+        double b = 2.71828;
+        String c = "Computer Science";
+
+        System.out.println(" ");
+        System.out.println(" ");
+        
+
+        System.out.println("This is room # " + a);
+        System.out.println("e is close to " + b);
+        System.out.println("I am learning a bit about " + c);
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        String sci = "Mr. Annas";
+        String pe = "Mr. Konsmo";
+        String ss = "Mr. Mazo";
+        String gui = "Mr. Robertson";
+
+        String math = "Mr. Tam";
+        String fr = "Mme. Begg";
+        String eng = "Mr. Tryssenaar";
+        String comp = "Mr. Zaremba";
+
+        String sci10 = "Science 10";
+        String pe10 = "PE 10";
+        String ss10 = "Socials 10";
+        String gui11 = "Guitar 11";
+
+        String math10 = "Math 10";
+        String fr10 = "French 10";
+        String eng10 = "English 10";
+        String comp11 = "Mr. Zaremba";
+
+        System.out.println(" ----------------------------------------");
+        System.out.println("| 1-1 |    "+ sci10 + " " + "|        " + sci + " |");
+        System.out.println("| 1-2 |        " + pe10  + "  |       " + pe + " |");
+        System.out.println("| 1-3 |    " + ss10 + " |         " + ss + " |");
+        System.out.println("| 1-4 |     " + gui11  + " |    " + gui + " |");
+
+        System.out.println("| 2-1 |       " + math10 + " |          " + math + " |");
+        System.out.println("| 2-2 |     " + fr10 + " |        "+ fr + " |");
+        System.out.println("| 2-3 |    " + eng10 + " |   "+ eng + " |");
+        System.out.println("| 2-4 |  " + comp11 + "  |      " + comp + " |");
+        System.out.println(" ----------------------------------------");
+
+    }
+}
