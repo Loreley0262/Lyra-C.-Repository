41377c16625c1f3e30931792801d262da26774ae
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2019-10-07 15:02:50 -0700

    asdf

---
+++ ! CEMC TESTS!!!/Junior 2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        System.out.println("hello");
+        System.out.println(n);
+
+        if (n>=1 && n<=100){
+            System.out.println("yeeee");
+        }
+
+        else{
+            System.out.println("noooooon");
+        }
+
+    }
+}
---
+++ New folder/New folder/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] nums = new int[4];
+
+        nums[0] = scan.nextInt();
+        nums[1] = scan.nextInt();
+        nums[2] = scan.nextInt();
+        nums[3] = scan.nextInt();
+
+        if (((nums[0] == 9) || (nums[0] == 8)) && ((nums[3] == 8) || (nums[3] == 9)) && (nums[1] == nums[2])) {
+            System.out.println("ignore");
+        }
+
+        else {
+            System.out.println("answer");
+        }
+
+    }
+}
