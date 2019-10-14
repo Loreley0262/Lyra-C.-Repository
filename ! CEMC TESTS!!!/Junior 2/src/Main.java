f23b2f673c8ad6261bbffbec871c6314eca3ad74
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2019-10-14 16:11:10 -0700

    asdf

--- ! CEMC TESTS!!!/Junior 2/src/Main.java
+++ ! CEMC TESTS!!!/Junior 2/src/Main.java
@@ -2,21 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
-        int n = scan.nextInt();
+//        numOfSpaces = NUMBER OF SPACES
+//        byeByeYesterday = YESTERDAY
+//        hiHiToday = TODAY
 
-        System.out.println("hello");
-        System.out.println(n);
+        System.out.println("How many parking spaces are in the row in total? There must be at least 1 space and less than 100 spaces");
+        int numOfSpaces = scan.nextInt(); //NUM OF SPACES
 
-        if (n>=1 && n<=100){
-            System.out.println("yeeee");
+        while (numOfSpaces<1 || numOfSpaces>100) {
+            System.out.println("no retry your num.");
         }
 
-        else{
-            System.out.println("noooooon");
+        System.out.println("Using the character \"C\" for filled spaces and \".\" for empty spaces," +
+                " input the status of the parking lot as it was yesterday.");
+        String byeByeYesterday = scan.next();
+
+        if (byeByeYesterday.length()!= numOfSpaces){
+            System.out.println("nonon retry");
         }
 
+
+        System.out.println("Using the character \"C\" for filled spaces and \".\" for empty spaces," +
+                " input the status of the parking lot as it is today.");
+        String hiHiToday = scan.next();
+
+        for (int i=0; i<numOfSpaces; i++){
+            //somehow search the string for both places w/C
+        }
     }
 }
--- !! arraays cuz y not/src/rpsButString.java
+++ !! arraays cuz y not/src/rpsButString.java
@@ -3,47 +3,107 @@ import java.util.Scanner;
 public class rpsButString {
     public static void main(String[] args) {
 
-        Scanner player = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        boolean asdf = true;
+        boolean fdsa = false;
+        int uWins = 0;
+        int uLosses = 0;
+        int nono = 0;
 
-        int com = (int)(Math.random()*2 + 1);
-
-        String[] rps = {"rock", "paper", "scissors"};
-
-        String npcChoice = rps[com];
-        System.out.println(npcChoice);
-
-
-            System.out.println("Choose rock, paper, or scissors");
-            String yourChoice = player.next();
+        while (asdf) {
+            System.out.println("Choose rock, paper, or scissors:");
+            String yourChoice = scan.next();
             System.out.println("You chose:  " + yourChoice);
 
-            if (npcChoice.equals(yourChoice))
-                System.out.println("TIE");
+            int com = (int)(Math.random()*2 + 1);
+            String[] rps = {"rock", "paper", "scissors"};
+            String npcChoice = rps[com];
+
+            if (npcChoice.equals(yourChoice)) {
+                System.out.println("You tied! How odd... Try again!");
+                fdsa = true;
+            }
 
             else if (npcChoice.equals("rock") && yourChoice.equals("paper")) {
-                System.out.println("it is rock");
-                System.out.println("you win"); }
+                System.out.println("Com chose:  Rock");
+                System.out.println("You WIN! YAY!");
+                uWins++;
+                System.out.println("You have " + uWins +" wins and " + uLosses + " losses.");
+                fdsa = true;}
             else if (npcChoice.equals("rock") && yourChoice.equals("scissors")){
-                System.out.println("it is rock");
-                System.out.println("You lose"); }
+                System.out.println("Com chose:  Rock");
+                System.out.println("You lose... Try again! Positive vibes are coming your way!");
+                uLosses++;
+                System.out.println("You have " + uWins +" wins and " + uLosses + " losses.");
+                fdsa = true;}
 
             else if (npcChoice.equals("paper") && yourChoice.equals("scissors")){
-                System.out.println("it is paper");
-                System.out.println("you win"); }
+                System.out.println("Com chose:  Paper");
+                System.out.println("You WIN! YAY!");
+                uWins++;
+                System.out.println("You have " + uWins +" wins and " + uLosses + " losses.");
+                fdsa = true;}
             else if (npcChoice.equals("paper") && yourChoice.equals("rock")){
-                System.out.println("it is paper");
-                System.out.println("you lose"); }
+                System.out.println("Com chose:  Paper");
+                System.out.println("You lose... Try again! Positive vibes are coming your way!");
+                uLosses++;
+                System.out.println("You have " + uWins +" wins and " + uLosses + " losses.");
+                fdsa = true;}
 
-            else if (npcChoice.equals("scissors") && yourChoice.equals("paper")){
-                System.out.println("it is scissors");
-                System.out.println("you lose"); }
             else if (npcChoice.equals("scissors") && yourChoice.equals("rock")){
-                System.out.println("it is scissors");
-                System.out.println("you win"); }
+                System.out.println("Com chose:  Scissors");
+                uWins++;
+                System.out.println("You have " + uWins +" wins and " + uLosses + " losses.");
+                System.out.println("You WIN! YAY!");
+                fdsa = true;}
+            else if (npcChoice.equals("scissors") && yourChoice.equals("paper")){
+                System.out.println("Com chose:  Scissors");
+                System.out.println("You lose... Try again! Positive vibes are coming your way!");
+                uLosses++;
+                System.out.println("You have " + uWins +" wins and " + uLosses + " losses.");
+                fdsa = true;}
 
             else {
-                System.out.println("eerroorrsssss");
+                System.out.println("Invalid response. Please try again.");
+                asdf = true;
+                fdsa = false;
+                nono++;
+            }
+
+            while (fdsa){
+                System.out.println("Would you like to play again? (y/n)");
+                String yOrN = scan.next();
+
+                if (yOrN.toLowerCase().equals("y")){
+                    asdf = true;
+                    fdsa = false; }
+
+                else if (yOrN.toLowerCase().equals("n")){
+                    System.out.println("Thank you for playing! \n" +
+                            " You have " + uWins +" wins and " + uLosses + " losses.");
+                    if (uWins == uLosses){
+                        System.out.println("You tied with the computer! Nice job!"); }
+                    else if (uWins>uLosses){
+                        System.out.println("You beat the computer! Congratulations!"); }
+                    else if (uWins<uLosses){
+                        System.out.println("You lost against the computer. But DON'T GIVE UP! POSITIVITY!"); }
+
+                    if (nono>0){
+                        if (nono>=3){
+                            System.out.println("You also input invalid responses " + nono + " times. " +
+                                    "\n \n \t \t \t You're not illiterate, so do better next time you dumdum!"); }
+                        else {
+                            System.out.println("You also input invalid responses " + nono + " times. "); } }
+                    else{
+                        System.out.println("You input invalid responses " + nono + " times. Nice typing skills!"); }
+                    asdf = false;
+                    fdsa = false; }
+                else{
+                    System.out.println("Invalid response. Please try again.");
+                    fdsa = true;
+                    nono++; }
             }
         }
+    }
 }
 
