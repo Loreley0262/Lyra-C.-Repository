b1ca9b0255ca490f46eab183930b1c01f50e677c
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-01-29 11:33:11 -0800

    asdf

--- (16) Time Class/src/Main.java
+++ (16) Time Class/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Time clock = new Time(23, 59, 59);  //made a Time object called "clock"
+        System.out.println(clock.getHour());
+
+
     }
 }
--- (16) Time Class/src/Time.java
+++ (16) Time Class/src/Time.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class Time {
+    Scanner scan = new Scanner(System.in);
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    Time(int hour, int minute, int second){    //School constructor creates new schools, each with a name, district, and id number.
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
 
     public int getHour() {
         return hour;
@@ -18,17 +31,37 @@ public class Time {
     public void setSecond(int second) {
         this.second = second;
     }
+    public void setTime(int hour, int minute, int second) {
+        System.out.println("input hour (24hr clock)");
+        this.hour = scan.nextInt();
+        System.out.println("input minute");
+        this.minute = scan.nextInt();
+        System.out.println("input second");
+        this.second = scan.nextInt();
 
-    private int hour;
-    private int minute;
-    private int second;
-
-
-
-    Time(int hour, int minute, int second){    //School constructor creates new schools, each with a name, district, and id number.
-        this.hour = hour;
-        this.minute = minute;
-        this.second = second;
+        if (this.second<0){
+            this.second = 0;
+        }
+        if (this.second>59){
+            this.second = 0;
+            this.minute++;
+        }
+        if (this.minute<0){
+            this.minute = 0;
+        }
+        if (this.minute>59){
+            this.minute = 0;
+            this.hour++;
+        }
+        if (this.hour<0){
+            this.hour = 0;
+        }
+        if (this.hour>23){
+            this.hour = 0;
+        }
     }
 
+    public String toString(){           //toString method prints the name and subject taught by the teacher
+        return getHour()+":"+getMinute()+":"+getSecond();
+    }
 }
