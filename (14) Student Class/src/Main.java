df21b505989c376e8d53afe1d4d734a3f3522b95
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-01-29 10:43:54 -0800

    asdf

--- (14) Student Class/src/Main.java
+++ (14) Student Class/src/Main.java
@@ -2,41 +2,22 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<Courses> bobCourses = new ArrayList<>();
-        bobCourses.add(new Courses("pe", 50));     // 0
-        bobCourses.add(new Courses("sci", 30));    // 1
-        bobCourses.add(new Courses("gui", 90));    // 2
-        bobCourses.add(new Courses("ss", 100));     // 3
         Student bob = new Student("Bob");
+        bob.addCourses();
+        bob.addCourses();
+        bob.addCourses();
+        bob.addCourses();
+        bob.printCourses();
         System.out.println(bob);
-        System.out.println(bobCourses.get(0));
 
-        ArrayList<Courses> johnCourses = new ArrayList<>();
-        johnCourses.add(new Courses("pe", 10));     // 0
-        johnCourses.add(new Courses("sci", 10));    // 1
-        johnCourses.add(new Courses("gui", 10));    // 2
-        johnCourses.add(new Courses("ss", 10));     // 3
         Student john = new Student("John");
+        john.addCourses();
         System.out.println(john);
-        System.out.println(johnCourses.get(2));
-
 
         System.out.println();
         System.out.println();
-        System.out.println((50+30+90+100) / 4);
-        int avg = 0;
+        System.out.println(bob.averageGrade());
 
-        //for (int i = 0; i<bobCourses.size(); i++){
-        //     avg = avg + ;
-        // }
-
-        System.out.println(bobCourses.size());
-        System.out.println(johnCourses); //in courses class, fix!
-
-/*        for (Test c : cars){ //colon is "in" so c is like i but for every car?
-            System.out.println(c);
-*/
     }
 
 }
--- (14) Student Class/src/Student.java
+++ (14) Student Class/src/Student.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 
 public class Student {
+    ArrayList<Courses> courses = new ArrayList<>();
+    int[] grades = new int[8];
 
     private static int iden;
     private String name;
-
+    int y =0;
 
     public int getIden() {
         return iden;
@@ -11,13 +14,31 @@ public class Student {
     public String getName() {
         return name;
     }
+    public void addCourses(){
+        int x = (int)(Math.random()*100-1);
+        courses.add(new Courses("course name",  x));
+        grades[y] = x;
+        y++;
+        System.out.println(x);
+        System.out.println(y);
+    }
+    public int averageGrade(){
+        int avg = 0;
+        for (int i=0; i<courses.size(); i++){
+            avg = avg + grades[i];
+        }
 
-
+        return avg/courses.size();
+    }
+    public void printCourses(){
+        for (int i=0; i<courses.size(); i++){
+            System.out.println(courses.get(i));
+        }
+    }
 
     public String toString(){
         return "Name: " + getName() + "\tID: " + getIden();
     }
-
 
     Student(String firstName){
         this.name = firstName;
