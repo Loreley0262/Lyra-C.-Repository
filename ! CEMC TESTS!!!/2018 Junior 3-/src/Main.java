098ac0bd72329030707a1b5e03dc9a8ff214b48c
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-01-31 10:35:31 -0800

    asdf

--- ! CEMC TESTS!!!/2018 Junior 3-/src/Main.java
+++ ! CEMC TESTS!!!/2018 Junior 3-/src/Main.java
@@ -4,30 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] city = new int[5];
-        int[] misc = new int[5];
         city[0]=0;
         for (int i=1; i<5; i++){
             city[i] = scan.nextInt();
         }
-        int k = 0;
-        for (int j=0; j<5; j++) {
-            for (int i = 0; i < 5; i++) {
-                if (i==k){
-                    misc[j]=0;
-                }
-                else{
-                    misc[j] = misc[j] + city[k] + city[i];
-                }
-                System.out.print(misc[j] + "\t");
-            }
-            System.out.println("\n");
-            k++;
-        }
+        
+
+
     }
 }
 /*
+i = city[i+1] - city[i]
+
   misc[j] = misc[j] + city[1] + city[i];
 
 0   3    10      12      5
 
 */
+
+
+//        int[] misc = new int[5];
+
+//        int k = 0;
+//        for (int j=0; j<5; j++) {
+//            for (int i = 0; i < 5; i++) {
+//                if (i==k){
+//                    misc[j]=0;
+//                }
+//                else{
+//                    misc[j] = misc[j] + city[k] + city[i];
+//                }
+//                System.out.print(misc[j] + "\t");
+//            }
+//            System.out.println("\n");
+//            k++;
+//        }
