a3ac71c0ebd88228fc1d65eb6b0d8221b583ab69
Loreley0262 <55299483+Loreley0262@users.noreply.github.com>
2020-02-20 11:35:12 -0800

    asdf

---
+++ (19) DataAbstractionAssignment/BankTests/Tests.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class Tests {
+    //make test obj (so customer?)
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void testPurchaseSeat(){
+
+    }
+
+    @Test
+    public void testSeatsRemaining(){
+
+    }
+
+    @Test
+    public void testIsAisle(){
+
+    }
+}
